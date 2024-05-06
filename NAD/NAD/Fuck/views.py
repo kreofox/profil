15a2e18966317b54a675157c41a56d1a9654f89b
David <80980510+kreofox@users.noreply.github.com>
2024-05-06 14:07:39 +0500

    path 0.1c

--- NAD/NAD/Fuck/views.py
+++ NAD/NAD/Fuck/views.py
@@ -4,5 +4,28 @@ def index(request):
     return HttpResponse("<h2>Home<h2>")
 def about(request):
     return HttpResponse("Site")
-def contact(request):
-    return HttpResponse("Conctact")
+def contact(request, name, age):
+    return HttpResponse(f"""
+            <h2>Contact<h2>
+            <p>Name: {name}</p>
+            <p>Age:{age}</p>
+     """)
+def https(request):
+    host = request.META["HTTP_HOST"]#получаем адрес сервера
+    user_agent= request.META["HTTP_USER_AGENT"] #получаем данные браузера
+    path = request.path #поулчаем запрошенный путь 
+    
+    return HttpResponse(f"""
+                <p>Host:{host}</p>
+                <p>Path:{path}</p>
+                <p>User-agent:{user_agent}</p>
+                """)
+#Set cookis
+def set(request):
+    #получаем из строки запрос имя пользователя
+    username = request.GET.get("username", "Undefined")
+    #создаем обьект ответ
+    response = HttpResponse(f"Hello {username}")
+    #передаем его куки
+    response.set_cookie("username", username)
+    return response
--- NAD/NAD/NAD/urls.py
+++ NAD/NAD/NAD/urls.py
@@ -2,7 +2,9 @@ from django.urls import path, re_path
 from Fuck import views
 
 urlpatterns = [
-    re_path(r'^about/contact/', views.contact),
+    re_path(r'^about/contact/', views.contact, kwargs={"name":"Tom", "age":38}),
     re_path(r'^about', views.about),
     path('', views.index),
+    path('https', views.https),
+    path('set', views.set)
 ]
